ac7e57a5366698f24a2bf31fddcf9ecc22787c36
Grauber157 <faria.dias@aluno.ifsp.edu.br>
2024-09-19 10:18:35 -0300

    a

--- core/sql.php
+++ core/sql.php
@@ -1,5 +1,5 @@
 <?php
-
+    //INSERT
     function Insert(string $entidade, array $dados) : string
     {
         $instrucao = "INSERT INTO {$entidade}";
@@ -13,6 +13,7 @@
         return $instrucao;
     }
 
+    //UPDATE
     function Update(string $entidade, array $dados, array $criterio = []) : string
     {
         $instrucao = "UPDATE {$entidade}";
@@ -37,5 +38,46 @@
         return $instrucao;
     }
 
-    
+    //DELETE
+    function Delete(string $entidade, array $criterio = []) : string
+    {
+        $instrucao = "DELETE {$entidade}";
+
+        if (!empty($criterio)) 
+        {
+            $instrucao .= ' WHERE ';
+
+            foreach($criterio as $expressao)
+            {
+                $instrucao .= ' ' . implode(' ', $expressao);
+            }
+        }
+        return $instrucao;
+    }
+
+    //SELECT
+    function Select(string $entidade, array $campos, array $criterio = [], string $ordem = null) : string
+    {
+        $instrucao = "SELECT " . implode(', ', $campos);
+        $instrucao .= " FROM {$entidade}";
+
+        if(!empty($criterio))
+        {
+            $instrucao .= ' WHERE ';
+
+            foreach($criterio as $expressao)
+            {
+                $instrucao .= ' ' . implode(' ', $expressao);
+            }    
+        }
+
+        if(!empty($ordem))
+        {
+            $instrucao .= " ORDER BY $ordem";
+        }
+
+        return $instrucao;
+    }
+
+
 ?>
